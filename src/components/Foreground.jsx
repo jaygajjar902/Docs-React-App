df28cd9bb79c254cc52a88aebdb0baf8438d5120
Jay Gajjar <jaygajjar902@gmail.com>
2024-02-16 12:34:04 +0530

    first commit

---
+++ src/components/Foreground.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-key */
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import { useRef } from 'react';
+import Card from './Card'
+
+
+function Foreground() {
+
+    // eslint-disable-next-line no-undef
+    const ref = useRef(null);
+
+    const data = [
+        {
+            desc: "This is working!",
+            filesize: ".9mb",
+            close: true,
+            tag: { isOpen: true, tagTitle: "Download", tagColor: "blue" },
+        },
+        {
+            desc: "Hello World!",
+            filesize: ".9mb",
+            close: false,
+            tag: { isOpen: false, tagTitle: "Delete", tagColor: "green" },
+        },
+        {
+            desc: "I'm a react developer now!",
+            filesize: ".9mb",
+            close: false,
+            tag: { isOpen: true, tagTitle: "Upload", tagColor: "green" },
+        },
+    ];
+
+  return (
+      <div ref={ref} className='fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5'>
+        {data.map((item, index)=>(
+            <Card data={item} reference={ref} />
+        ))}
+      </div>
+  );
+}
+
+export default Foreground
